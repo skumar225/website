78f4c9effbf3ab73f323bc7af1c6c977d1bd4b67
Shilpa Kumar <skumar225@gmail.com>
2017-06-01 19:09:56 -0700

    First commit

---
+++ src/public/js/main.js
@@ -0,0 +1,28 @@
+$(window).on("load", function(){
+//Scroll animation
+$('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $(this).attr('href') ).offset().top
+    }, 500);
+    return false;
+});
+
+ $(window).scroll( function(){
+    
+        /* Check the location of each desired element */
+        $('.hideme').each( function(i){
+            
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({'opacity':'1'},100);
+                    
+            }
+            
+        }); 
+    
+    });
+});

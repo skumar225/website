9270763295e4e87df75fdb4b4837bbc5b2506dab
skumar225 <skumar225@gmail.com>
2017-06-05 14:59:30 -0700

    port console message added

--- src/app.js
+++ src/app.js
@@ -39,5 +39,5 @@ app.get('/blog/:title?', function(req, res) {
 });
 
 app.listen(process.env.PORT || 3000, function() {
-	console.log("The frontend server is running on port 3000!");
+	console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });

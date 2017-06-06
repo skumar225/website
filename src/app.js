6b826f183ca992325732ae7b5e8e6705eaf67c3b
skumar225 <skumar225@gmail.com>
2017-06-05 20:19:53 -0700

    using app

--- src/app.js
+++ src/app.js
@@ -39,6 +39,6 @@ app.get('/blog/:title?', function(req, res) {
 	}
 });
 
-server.listen(process.env.PORT || 3000, function() {
+app.listen(process.env.PORT || 3000, function() {
 	console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
